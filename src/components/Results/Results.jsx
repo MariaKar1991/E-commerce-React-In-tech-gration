d9a8769fa5115576ab61900485dee5fc8b011694
Judi-Ali <itprogrammer20@gmail.com>
2024-03-21 21:54:02 -0700

    unrequired-fragment

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -3,11 +3,9 @@ import PropTypes from "prop-types";
 
 export default function Results({ numResults }) {
   return (
-    <>
-      <h2 className="title">
-        Bags<span>{numResults} results</span>
-      </h2>
-    </>
+    <h2 className="title">
+      Bags<span>{numResults} results</span>
+    </h2>
   );
 }
 
